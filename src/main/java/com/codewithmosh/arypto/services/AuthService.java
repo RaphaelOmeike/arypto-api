1f9b1f4b664a06eab74a2ec4044872369dc733b1
Raphael Omeike <raphaelomeike1000@gmail.com>
2025-07-31 22:54:30 +0100

    resolved jwt dependency issues

--- src/main/java/com/codewithmosh/arypto/services/AuthService.java
+++ src/main/java/com/codewithmosh/arypto/services/AuthService.java
@@ -1,7 +1,7 @@
 package com.codewithmosh.arypto.services;
 
-import com.codewithmosh.store.entities.User;
-import com.codewithmosh.store.repositories.UserRepository;
+import com.codewithmosh.arypto.entities.User;
+import com.codewithmosh.arypto.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -12,7 +12,7 @@ public class AuthService {
     private final UserRepository userRepository;
     public User getCurrentUser() {
         var authentication = SecurityContextHolder.getContext().getAuthentication();
-        var userId = (Long) authentication.getPrincipal();
+        var userId = (String) authentication.getPrincipal();
 
         return userRepository.findById(userId).orElse(null);
     }
--- src/main/java/com/codewithmosh/arypto/services/UserService.java
+++ src/main/java/com/codewithmosh/arypto/services/UserService.java
@@ -1,8 +1,8 @@
-package com.codewithmosh.arypto.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService implements UserDetailsService {
-
-}
+//package com.codewithmosh.arypto.services;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class UserService implements UserDetailsService {
+//
+//}
