c596f3599efd437b5b801c71eb67463c1244de9d
Raphael Omeike <raphaelomeike1000@gmail.com>
2025-08-02 21:25:00 +0100

    integrated the WebhookController

--- src/main/java/com/codewithmosh/arypto/controllers/WebhookController.java
+++ src/main/java/com/codewithmosh/arypto/controllers/WebhookController.java
@@ -1,11 +1,83 @@
 package com.codewithmosh.arypto.controllers;
 
 
+import com.codewithmosh.arypto.dtos.ErrorDto;
+import com.codewithmosh.arypto.exceptions.TransactionNotFoundException;
+import com.codewithmosh.arypto.exceptions.WalletNotFoundException;
+import com.codewithmosh.arypto.services.CryptoPaymentGateway;
+import com.codewithmosh.arypto.services.UtilityServiceGateway;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/webhook")
+@AllArgsConstructor
 public class WebhookController {
+    private final CryptoPaymentGateway paymentGateway;
+    private final UtilityServiceGateway serviceGateway;
+    //for testing
+    @PostMapping("/quidax")
+    public ResponseEntity<String> handleWebhook(HttpServletRequest request) throws IOException {
+        StringBuilder jsonBuilder = new StringBuilder();
+        try (BufferedReader reader = request.getReader()) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonBuilder.append(line);
+            }
+        }
 
+        String rawPayload = jsonBuilder.toString();
+        paymentGateway.processWebhook(rawPayload);
+
+        return ResponseEntity.ok("Webhook processed");
+    }
+
+
+    @PostMapping("/vtpass")
+    public Map<String, String> handleVTPassWebhook(HttpServletRequest request) throws IOException {
+        StringBuilder jsonBuilder = new StringBuilder();
+        try (BufferedReader reader = request.getReader()) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonBuilder.append(line);
+            }
+        }
+        String rawPayload = jsonBuilder.toString();
+        serviceGateway.processWebhookRequest(rawPayload);
+
+        return Map.of("response", "success");
+    }
+
+    @ExceptionHandler(TransactionNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleTransactionNotFound(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorDto(ex.getMessage()));
+    }
+
+    @ExceptionHandler(WalletNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleWalletNotFound(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorDto("Wallet not found: "));
+    }
+
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<ErrorDto> handleIO(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorDto(ex.getMessage()));
+    }
 }
